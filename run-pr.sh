405c9d6740b3e839a3addf2bac130cab317eadd2
yuke wang <505008605@qq.com>
2019-09-29 16:28:37 -0700

    small modify

---
+++ run-pr.sh
@@ -0,0 +1,11 @@
+echo -e "COLLAB\n"
+./pr --input datasets/COLLAB_A.txt
+
+echo -e "IMDB-BINARY"
+./pr --input datasets/IMDB-BINARY_A.txt
+
+echo  -e "REDDIT"
+./pr --input datasets/reddit_A.txt
+
+echo -e "REDDIT_MULTI-12K"
+./pr --input datasets/REDDIT_MULTI-12K_A.txt
